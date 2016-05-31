fc474d72c4cf8a81f39be5d31d0a5c9738f56ecb
AviadorC <patryk.romanczuk@gmail.com>
2016-05-31 16:42:53 +0200

    Initial commit
    Prepared sample solution

---
+++ HelloXamarin/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloXamarin
+{
+    [Activity(Label = "HelloXamarin", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Holo.NoActionBar")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = Resources.GetString(Resource.String.ButtonClicked); };
+        }
+    }
+}
+
